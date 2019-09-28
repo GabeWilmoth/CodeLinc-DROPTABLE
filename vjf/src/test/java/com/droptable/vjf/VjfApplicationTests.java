23d12a7b53868f96a8d41127b92149cbe7d6f8d8
GabeWilmoth <gawilmot@uncg.edu>
2019-09-28 18:25:36 -0400

    Working Build

---
+++ vjf/src/test/java/com/droptable/vjf/VjfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.droptable.vjf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VjfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
